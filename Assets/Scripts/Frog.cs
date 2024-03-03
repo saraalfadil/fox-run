06483639c6ebd2ad7521eea8ce8f51d6a014f31e
Sara Alfadil <saraalfadil7@gmail.com>
2024-03-03 11:28:36 -0500

    Refactored Enemy components

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class Frog : Enemy
 {
-    private Collider2D coll;
     [SerializeField] private Transform leftCap;
     [SerializeField] private Transform rightCap;
     [SerializeField] private float jumpLength = 10f;
     [SerializeField] private float jumpHeight = 15f;
-    [SerializeField] private LayerMask ground;
-    private bool facingLeft = true;
+	private bool facingLeft;
 
     protected override void Start()
     {
         base.Start();
-        coll = GetComponent<Collider2D>();
     }
 
     private void Update()
@@ -38,7 +35,7 @@ public class Frog : Enemy
         // Transition from fall to idle
         if (anim.GetBool("falling"))
         {
-            if (coll.IsTouchingLayers(ground))
+            if (isTouchingGround)
             {
                 anim.SetBool("falling", false);
                 anim.SetBool("jumping", false);
@@ -46,50 +43,56 @@ public class Frog : Enemy
         }
 	}
 
-    private void Move()
-    {
-        if (facingLeft)
-        {
-            // Check if we are past the left cap
-            if (IsPastLeftCap(leftCap))
-            {
-                // Check if sprite is facing right
-                if (!IsFacingRight())
-                {
-                    // Face the right direction
-                    FaceRight();
-                }
-                JumpLeft();
-            }
-            else
-            {
-                facingLeft = false;
-            }
-        }
-        else
-        {
-            // Check if we are past the right cap
-            if (IsPastRightCap(rightCap))
-            {
-                // Check if sprite is facing left
-                if (!IsFacingLeft())
-                {
-                    // Face the left direction
-                    FaceLeft();
-                }
-                JumpRight();
-            }
-            else
-            {
-                facingLeft = true;
-            }
-        }
-    }
+	private void Move()
+	{
+		if (facingLeft)
+		{
+			MoveLeft();
+		}
+		else
+		{
+			MoveRight();
+		}
+	}
+
+	private void MoveLeft()
+	{
+ 		// Check if we are past the left cap
+		if (IsPastLeftCap(leftCap))
+		{
+			// Check if sprite is facing right
+			if (!IsFacingRight())
+			{
+				// Face the right direction
+				FaceRight();
+			}
+			JumpLeft();
+		}
+		else
+		{
+			facingLeft = false;
+		}
+	}
+
+	private void MoveRight()
+	{
+		if (IsPastRightCap(rightCap))
+		{
+			if (!IsFacingLeft())
+			{
+				FaceLeft();
+			}
+			JumpRight();
+		}
+		else
+		{
+			facingLeft = true;
+		}
+	}
 
 	private void JumpLeft()
 	{
-		// If touching ground, then jump
-		if (coll.IsTouchingLayers(ground))
+		if (isTouchingGround)
 		{
 			rb.velocity = new Vector2(-jumpLength, jumpHeight);
 			anim.SetBool("jumping", true);
@@ -98,8 +101,7 @@ public class Frog : Enemy
 
 	private void JumpRight()
 	{
-		// If touching ground, then jump
-		if (coll.IsTouchingLayers(ground))
+		if (isTouchingGround)
 		{
 			rb.velocity = new Vector2(jumpLength, jumpHeight);
 			anim.SetBool("jumping", true);
