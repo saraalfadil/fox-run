bc29c176abb9f8be196e27dcdd9357dd91643ec4
Sara Alfadil <saraalfadil7@gmail.com>
2024-05-19 12:49:44 -0400

    Created fish prefab

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -6,33 +6,6 @@ using UnityEngine;
 public class Fish : Enemy
 {
 
-	/*[SerializeField] private float speed;
-    [SerializeField] private int startingPoint;
-    [SerializeField] private Transform[] points;
-    private int i;
-
-    private void Start()
-    {
-        transform.position = points[startingPoint].position;
-    }
-
-    private void Update()
-    {
-        // check the distance of the platform and the point
-        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
-        {
-            i++;
-
-            // check if the platform was on the last point after the index increase
-            if(i == points.Length)
-            {
-                i = 0;
-            }
-        }
-
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
-    }*/
-
     [SerializeField] private Transform upCap;
     [SerializeField] private Transform downCap;
     [SerializeField] private float swimLength = 2f;
@@ -45,7 +18,6 @@ public class Fish : Enemy
 	
 	private void Move()
 	{
-		Debug.Log("Move!!");
 		if (facingUp)
 		{
 			MoveUp();
@@ -58,13 +30,8 @@ public class Fish : Enemy
 	
 	private void MoveDown()
 	{
-		Debug.Log("MoveDown!!");
-		Debug.Log("UP CAP Y POSITION: " + upCap.localPosition.y);
-		Debug.Log("FISH Y POSITION: " + transform.localPosition.y);
 		if (IsPastDownCap(downCap))
 		{
-			Debug.Log("IsPastUpCap!!");
-
 			// Check if sprite is facing down
 			if (!IsFacingDown())
 			{
@@ -81,13 +48,8 @@ public class Fish : Enemy
 
 	private void MoveUp()
 	{
-		Debug.Log("MoveUp!!");
-		Debug.Log("DOWN CAP Y POSITION: " + downCap.localPosition.y);
-		Debug.Log("FISH Y POSITION: " + transform.localPosition.y);
 		if (IsPastUpCap(upCap))
 		{
-			Debug.Log("IsPastDownCap!!");
-			
 			// Check if sprite is facing up
 			if (!IsFacingUp())
 			{
@@ -103,7 +65,6 @@ public class Fish : Enemy
 
 	private void SwimUp()
 	{
-		Debug.Log("SWIM UP");
 		rb.velocity = new UnityEngine.Vector2(rb.velocity.x, swimLength);
 	}
 
