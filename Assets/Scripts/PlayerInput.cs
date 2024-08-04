a31e4ab24c6256b77dc98e5c53fcf9a397b87f3f
Sara Alfadil <saraalfadil7@gmail.com>
2024-08-03 22:19:19 -0400

    Added Xbox controller support using new Unity InputSystem

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
 	[SerializeField] PlayerController playerController;
 	
+	InputMaster controls;
+	private Vector2 moveInput;
+
+	public void Awake()
+	{
+		controls = new InputMaster();
+	}
+
+	public void OnEnable()
+	{
+		controls.Player.Enable();
+	}
+
+	public void OnDisable()
+	{
+		controls.Player.Disable();
+	}
+
+	void FixedUpdate()
+	{
+		moveInput = controls.Player.Movement.ReadValue<Vector2>();
+	}
+
     private void Update()
     {
 
 		if (playerController == null)
 			return;
 
-		// Ignore input if player is hurt or level has ended
+		// Ignore input if player is hurt, level has ended, or player is using the slide
         if (playerController.isHurt || PermanentUI.perm.endLevel || playerController.enteredSlide)
             return;
 
@@ -20,11 +45,11 @@ public class PlayerInput : MonoBehaviour
         float vDirection = Input.GetAxis("Vertical");
 		bool jumpButton = Input.GetButtonDown("Jump");
 
-        if (hDirection < 0)
+        if (hDirection < 0 || moveInput.x < 0)
         {
             playerController.movementScript.MovePlayerLeft();
         }
-        else if (hDirection > 0)
+        else if (hDirection > 0 || moveInput.x > 0)
         {
             playerController.movementScript.MovePlayerRight();
         }
@@ -40,5 +65,14 @@ public class PlayerInput : MonoBehaviour
         }
 
     }
+
+	// Mapped to the "jump" input action
+	public void OnJump() 
+	{
+		if (playerController.isTouchingGround) 
+		{
+			playerController.movementScript.Jump();
+		}
+	}
 
 }
